18c5391086406511de9c25dc0174d806fedcaf8f
Sailesh2577 <42528517+Sailesh2577@users.noreply.github.com>
2018-08-23 11:31:56 -0500

    Hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Caleb Jacobsen  Sailesh Pujara
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc, char **argv)
+ {
+  printf("Sailesh Pujara majoring in Computer Science \n");
+
+  return 0;
+}
